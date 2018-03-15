7c0fa2972d5b0b2fba2787abb28b7977f1679759
bilo <bilo.lwabona@gmail.com>
2018-03-15 22:51:03 +0200

    seperated logic from cli

---
+++ index.js
@@ -0,0 +1,19 @@
+const commands = require('./lib/gitCommands.json')
+
+export const gititOutput = (commandArgs) => {
+    console.log('printing: ', commandArgs)
+    // console.log('commands: ', commands)
+    let output = commandArgs.join(' ')
+    if(commandArgs.length == 1 || true) {
+        output = commands[commandArgs[0]] ? commands[commandArgs[0]].effect : ''
+    }
+    return output
+}
+
+export const gitit = (command) => {
+    return gititOutput(command.split(' '))
+}
+
+module.exports = {
+    gitit: gitit
+}
